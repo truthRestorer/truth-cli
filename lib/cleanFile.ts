a2815879e1d72902358da93a8b0b4f03a04c130b
Plumbiu <3434909403@qq.com>
2023-08-01 15:24:26 +0800

    fix: 🧩 add await

--- lib/cleanFile.ts
+++ lib/cleanFile.ts
@@ -6,5 +6,5 @@ const files = ['graph.json', 'tree.json', 'relations.json']
 
 export async function cleanFiles() {
   for (let i = 0; i < files.length; i++)
-    fs.unlink(path.resolve(webPath, `./${files[i]}`))
+    await fs.unlink(path.resolve(webPath, `./${files[i]}`))
 }
