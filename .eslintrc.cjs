413aac87369ceea7dad0f165c5b16fddce7bccfb
Plumbiu <3434909403@qq.com>
2023-08-04 19:24:58 +0800

    style: 🎨 加入eslint校验

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,16 @@
 module.exports = {
-  extends: '@antfu',
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', '@antfu'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'linebreak-style': ['error', 'windows'],
+  },
 }
