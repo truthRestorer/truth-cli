5bd0da4b520f1cdc6c16568355ef67fb3997b753
Plumbiu <3434909403@qq.com>
2023-08-09 20:49:33 +0800

    build: 📦️ 完善build脚本

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,5 +1,7 @@
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
+import process from 'node:process'
+import fs from 'node:fs/promises'
 import { build } from 'vite'
 import type { ModuleFormat } from 'rollup'
 import typescript from '@rollup/plugin-typescript'
@@ -7,31 +9,57 @@ import commonjs from '@rollup/plugin-commonjs'
 import terser from '@rollup/plugin-terser'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import { rollup } from 'rollup'
+import minimist from 'minimist'
 
+const argv = minimist(process.argv.slice(2))
 const __dirname = fileURLToPath(new URL('.', import.meta.url))
+const packagesDir = path.resolve(__dirname, '../packages/')
+
+async function buildOptions() {
+  const dirs = await fs.readdir(packagesDir)
+  const plugins = [
+    nodeResolve({
+      preferBuiltins: true,
+      exportConditions: ['node'],
+    }),
+    typescript({
+      exclude: ['packages/web/**/*.ts', '**/__test__/**', 'node_modules/**'],
+    }),
+    commonjs(),
+    terser(),
+  ]
+  const opts: any = {}
+  for (let i = 0; i < dirs.length; i++) {
+    if (dirs[i] !== 'web') {
+      opts[dirs[i]] = {
+        input: {
+          input: path.resolve(__dirname, `../packages/${dirs[i]}/index.ts`),
+          plugins,
+        },
+        output: {
+          dir: path.resolve(__dirname, `../packages/${dirs[i]}/dist`),
+          format: 'es' as ModuleFormat,
+        },
+      }
+    }
+  }
+  return opts
+}
 
 async function buildCli() {
-  const inputOptions = {
-    input: path.resolve(__dirname, '../packages/cli/bin/index.ts'),
-    plugins: [
-      nodeResolve({
-        preferBuiltins: true,
-        exportConditions: ['node'],
-      }),
-      typescript({
-        exclude: ['packages/web/**/*.ts', '**/__test__/**', 'node_modules/**'],
-      }),
-      commonjs(),
-      terser(),
-    ],
-  }
+  const { cli } = await buildOptions()
   const outputOptions = {
-    dir: path.resolve(__dirname, '../packages/cli/dist'),
-    format: 'es' as ModuleFormat,
+    ...cli.output,
     banner: '#! /usr/bin/env node',
   }
-  const bundle = await rollup(inputOptions)
+  const bundle = await rollup(cli.input)
   await bundle.write(outputOptions)
+}
+
+async function buildShared() {
+  const { shared } = await buildOptions()
+  const bundle = await rollup(shared.input)
+  await bundle.write(shared.output)
 }
 
 async function buildWeb() {
@@ -47,8 +75,17 @@ async function buildWeb() {
 }
 
 async function resolveBuild() {
-  await buildWeb()
-  await buildCli()
+  if (argv.cli) {
+    await buildCli()
+  }
+  else if (argv.web) {
+    await buildWeb()
+  }
+  else {
+    await buildShared()
+    await buildWeb()
+    await buildCli()
+  }
 }
 
 resolveBuild()
