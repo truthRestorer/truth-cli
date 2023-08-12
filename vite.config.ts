92c3007391fa2a6e4a38c8c414fd49c170f57e42
Plumbiu <3434909403@qq.com>
2023-08-12 21:46:50 +0800

    test: ✅ 根目录测试

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,10 @@ import { viteSingleFile } from 'vite-plugin-singlefile'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), viteSingleFile()],
+  test: {
+    exclude: ['node_modules/**'],
+    globals: true,
+    // simulate DOM with happy-dom
+    environment: 'happy-dom',
+  },
 })
