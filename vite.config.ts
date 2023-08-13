da789798bc32b25458afa2450ff0348b5cf67491
Plumbiu <3434909403@qq.com>
2023-08-13 13:23:59 +0800

    fix: 🧩 自定义标签导致原有标签无效

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,7 @@ import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue({
-    template: {
-      compilerOptions: {
-        isCustomElement: tag => tag === 'json-viewer',
-      },
-    },
-  }), viteSingleFile()],
+  plugins: [vue(), viteSingleFile()],
   test: {
     exclude: ['node_modules/**'],
     globals: true,
