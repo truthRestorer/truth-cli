2e625a13686345c56940f14aefa5cfbabea2933f
Plumbiu <3434909403@qq.com>
2023-08-13 17:21:15 +0800

    fix: 🧩 vercel assets 文件夹找不到问题

--- scripts/build-playground.ts
+++ scripts/build-playground.ts
@@ -1,19 +1,18 @@
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
-
-// import { genJSONFile, genTxtFile, genWebFile } from '../packages/cli/src/genFile.js'
+import { genJSONFile, genTxtFile, genWebFile } from '../packages/cli/src/genFile.js'
 import { buildWeb } from './utils.js'
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 async function createPlaygroundServer() {
-  // const playgroundAssetsPath = path.resolve(__dirname, '../playground/src/assets')
-  // await genWebFile({
-  //   dep: 3,
-  //   writePath: playgroundAssetsPath,
-  // })
-  // await genJSONFile(3, playgroundAssetsPath, true)
-  // await genTxtFile(3, playgroundAssetsPath, true)
+  const playgroundAssetsPath = path.resolve(__dirname, '../playground/src/assets')
+  await genWebFile({
+    dep: 3,
+    writePath: playgroundAssetsPath,
+  })
+  await genJSONFile(3, playgroundAssetsPath, true)
+  await genTxtFile(3, playgroundAssetsPath, true)
   await buildWeb({
     isDeploy: true,
     buildPath: path.resolve(__dirname, '../playground/dist'),
