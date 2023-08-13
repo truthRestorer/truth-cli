fadfc90f3f4b09073ee671e17d4c8dc12ee08437
Plumbiu <3434909403@qq.com>
2023-08-13 11:16:12 +0800

    build: 📦️ 完善dev命令

--- scripts/dev-cli.ts
+++ scripts/dev-cli.ts
@@ -5,18 +5,22 @@ import { genByCommand } from '../packages/cli/src/index.js'
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
-async function createCliServer() {
-  await genByCommand({ dep: 3, isDev: true, isBoth: true })
+export async function createViteServer() {
   const server = await createServer({
-    // 任何合法的用户配置选项，加上 `mode` 和 `configFile`
     configFile: path.resolve(__dirname, '../vite.config.ts'),
     root: path.resolve(__dirname, '../packages/web'),
     server: {
       port: 1337,
     },
   })
-  await server.listen()
 
+  return server
+}
+
+async function createCliServer() {
+  await genByCommand({ dep: 3, isDev: true, isBoth: true })
+  const server = await createViteServer()
+  await server.listen()
   server.printUrls()
 }
 
