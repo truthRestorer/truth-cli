6ce11323f2cb483e9313b1c2c05276a00aa9f28d
Plumbiu <3434909403@qq.com>
2023-08-15 11:12:13 +0800

    fix(web): 删除无用数据

--- packages/web/src/components/JsonView.vue
+++ packages/web/src/components/JsonView.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
 import type { CSSProperties } from 'vue'
-import { ref } from 'vue'
 import VueJsonPretty from 'vue-json-pretty'
 import 'vue-json-pretty/lib/styles.css'
 
 defineProps(['data'])
 
-const selectedValue = ref('')
 const style: CSSProperties = {
   overflow: 'hidden',
 }
@@ -19,7 +17,6 @@ const style: CSSProperties = {
     :style="style"
     :show-line="false"
     :show-double-quotes="false"
-    :selected-value="selectedValue"
     show-line-number
   />
 </template>
