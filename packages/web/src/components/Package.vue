9acf843ec6d8e5804f1a9566fccd499967965258
Plumbiu <3434909403@qq.com>
2023-08-15 10:00:29 +0800

    style(web): 按钮文字改为切换图表

--- packages/web/src/components/Package.vue
+++ packages/web/src/components/Package.vue
@@ -53,7 +53,7 @@ onUnmounted(() => {
         </template>
       </ElInput>
       <ElButton @click="lengend = c.toggleLegend(lengend)">
-        切换实例
+        切换图表
       </ElButton>
       <div class="link">
         <a target="_blank" href="https://truth-cli-playground.vercel.app/">演练场</a>
@@ -83,32 +83,34 @@ onUnmounted(() => {
   top: 0;
   left: 0;
   right: 0;
-  background-color: #fefefe;
   display: flex;
   justify-content: space-between;
+  background-color: #fefefe;
   padding: 8px 40px;
   max-height: 60px;
   box-shadow: 0 1px 12px #efefef;
   z-index: 9999;
-}
-.right, .left, .link {
-  display: flex;
-  align-items: center;
-  gap: 25px;
+  & .right, .left{
+    display: flex;
+    align-items: center;
+    gap: 25px;
+  }
 }
 .link {
+  display: flex;
+  align-items: center;
   gap: 10px;
   min-width: max-content;
-}
-.link a {
-  color: #434343;
-  font-size: 14px;
-  text-decoration: none;
-  border-bottom: 2px solid transparent;
-  transition: all 0.35s;
-}
-.link a:hover {
-  border-color: #26a3eb;
-  color: #000;
+  & a {
+    color: #434343;
+    font-size: 14px;
+    text-decoration: none;
+    border-bottom: 2px solid transparent;
+    transition: border-color 0.25s;
+  }
+  & a:hover {
+    border-color: #75dcff;
+    color: #1a1b1b;
+  }
 }
 </style>
