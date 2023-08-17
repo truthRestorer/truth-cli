42961767400d3453dccf3216787910792c1db8dc
Plumbiu <3434909403@qq.com>
2023-08-17 23:30:48 +0800

    build: 简化build逻辑

--- scripts/build-vercel.ts
+++ scripts/build-vercel.ts
@@ -1,28 +1,25 @@
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import minimist from 'minimist'
-import { genPkgsFile, genWebFile } from '../packages/cli/src/index.js'
-import { buildWeb } from './utils.js'
+import { genPkgsFile } from '../packages/cli/src/index.js'
+import { buildWeb, genWebFile } from './utils.js'
 
 // eslint-disable-next-line n/prefer-global/process
 const argv = minimist(process.argv.slice(2))
 const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
+// 发布到 vercel 中去
 async function resolveBuild() {
   const {
     web: _web,
     path: _path,
     playground: _playground,
   } = argv
-  // 发布到 vercel 中去
-  if (_web) {
-    await genWebFile()
-  }
-  else if (_playground) {
-    const assetPath = path.resolve(__dirname, '../playground/src/assets')
-    await genWebFile(assetPath)
-    await genPkgsFile(3, 'txt', assetPath)
-    await genPkgsFile(3, 'json', assetPath)
+  const writePath = path.resolve(__dirname, `${_path}/${_web ? 'public' : '/src/assets'}`)
+  await genWebFile(writePath)
+  if (_playground) {
+    await genPkgsFile(3, 'txt', writePath)
+    await genPkgsFile(3, 'json', writePath)
   }
   await buildWeb(_path)
 }
--- scripts/dev.ts
+++ scripts/dev.ts
@@ -1,12 +1,12 @@
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { createServer } from 'vite'
-import { genWebFile } from '../packages/cli/src/index.js'
+import { genWebFile } from './utils.js'
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 async function createDevServer() {
-  await genWebFile()
+  await genWebFile(path.resolve(__dirname, '../packages/web/public'))
   const server = await createServer({
     configFile: path.resolve(__dirname, '../vite.config.ts'),
     root: path.resolve(__dirname, '../packages/web'),
