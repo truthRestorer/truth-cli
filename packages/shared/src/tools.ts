705b30aaf44c0bf776e53b7e4054da26426146f2
Plumbiu <3434909403@qq.com>
2023-08-18 18:21:56 +0800

    fix: 删除无用变量

--- packages/shared/src/tools.ts
+++ packages/shared/src/tools.ts
@@ -9,12 +9,3 @@ export function useEntries(obj: object | undefined | null) {
 export function isEmptyObj(obj: object | undefined | null) {
   return JSON.stringify(obj) === '{}' || obj === undefined || obj === null
 }
-
-/**
- * 处理依赖路径，\\转换成/，提取第一个node_modules之后的
- * @param path
- */
-export function transPath(path: string) {
-  const regPath = path.match(/node_modules(.*)/)
-  return regPath ? regPath[0].replace(/\\/g, '/') : ''
-}
