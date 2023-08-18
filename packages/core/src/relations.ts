86cf4e6914936b7d9f4dc83f865934cc3b4d6421
Plumbiu <3434909403@qq.com>
2023-08-18 16:14:07 +0800

    perf: ⚡️ 优化算法

--- packages/core/src/relations.ts
+++ packages/core/src/relations.ts
@@ -13,7 +13,6 @@ export function useReadFile(p: string) {
   const pkg = JSON.parse(json.toString())
   return pkg
 }
-
 /**
  * `truth-cli` 为了优化读文件的操作，选择了读取文件后形成一个 relations，后续文件的生成都依赖于这个 relations
  * 读取文件的速度很慢，`truth-cli` 只会读取一次(根目录和 node_modules 目录的 package.json)，形成一种对象格式
@@ -32,47 +31,20 @@ function dealPkg(p: string) {
   isEmptyObj(devDependencies) || (relations[name]!.devDependencies = devDependencies)
 }
 
-function dealPnpmModules(p: string) {
-  const dirs = useReadDir(`${p}/node_modules`)
-  for (let i = 0; i < dirs.length; i++) {
-    if (dirs[i][0] === '.')
-      continue
-    const nodePath = `${p}/node_modules/${dirs[i]}`
-    // 处理带有 @
-    if (dirs[i][0] === '@') {
-      const subDirs = useReadDir(nodePath)
-      for (let j = 0; j < subDirs.length; j++)
-        dealPkg(`${nodePath}/${subDirs[j]}/package.json`)
-    }
-    else {
-      dealPkg(`${nodePath}/package.json`)
-    }
-  }
-}
-
 /**
  * 读取 node_modules 目录下的所有 package.json 文件
  */
 function readGlob(p: string) {
-  const pkgsRoot = useReadDir(p)
-  for (let i = 0; i < pkgsRoot.length; i++) {
-    const pkgPath = resolve(p, `${pkgsRoot[i]}`)
-    if (pkgsRoot[i][0] === '.' && pkgsRoot[i] !== '.pnpm')
+  const dirs = useReadDir(p)
+  for (let i = 0; i < dirs.length; i++) {
+    const pkgPath = resolve(p, `${dirs[i]}`)
+    if ((dirs[i][0] === '.' || dirs[i] === 'lock.yaml') && dirs[i] !== '.pnpm')
       continue
     // 处理 .pnpm 文件
-    if (pkgsRoot[i] === '.pnpm') {
-      dealPnpmModules(pkgPath)
-    }
-    else {
-      // 处理带有 @
-      if (pkgsRoot[i][0] === '@') {
-        const dirs = useReadDir(pkgPath)
-        for (let i = 0; i < dirs.length; i++) readGlob(pkgPath)
-      }
-      else {
-        dealPkg(`${pkgPath}/package.json`)
-      }
-    }
+    if (fs.existsSync(`${pkgPath}/package.json`))
+      dealPkg(`${pkgPath}/package.json`)
+    else
+      readGlob(pkgPath)
   }
 }
 
