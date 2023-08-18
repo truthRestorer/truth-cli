67d1694c79ef015cae62d6627119d841fa33b2a7
Plumbiu <3434909403@qq.com>
2023-08-18 15:48:46 +0800

    fix: playground dev脚本

--- scripts/dev.ts
+++ scripts/dev.ts
@@ -1,17 +1,26 @@
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { createServer } from 'vite'
+import minimist from 'minimist'
 import { genWebFile } from './utils.js'
 
+// eslint-disable-next-line n/prefer-global/process
+const argv = minimist(process.argv.slice(2))
 const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 async function createDevServer() {
-  await genWebFile(path.resolve(__dirname, '../packages/web/public'))
+  const {
+    web: _web,
+    path: _path,
+    playground: _playground,
+  } = argv
+  const writePath = path.resolve(__dirname, `${_path}/${_web ? '/public' : '/src/assets'}`)
+  await genWebFile(writePath)
   const server = await createServer({
     configFile: path.resolve(__dirname, '../vite.config.ts'),
-    root: path.resolve(__dirname, '../packages/web'),
+    root: path.resolve(__dirname, _path),
   })
-  await server.listen()
+  await server.listen(_web ? 5173 : 1337)
   server.printUrls()
 }
 
