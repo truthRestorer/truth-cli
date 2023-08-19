49847eb1754e35f6bcb6aed34dfab176efe3a41c
Plumbiu <3434909403@qq.com>
2023-08-19 10:41:01 +0800

    fix(example): 完善使用说明

--- example/genGraph.ts
+++ example/genGraph.ts
@@ -6,15 +6,34 @@ import { relations } from './index.js'
 const graph1 = genGraph(relations)
 console.log(graph1)
 
-// 2. 自定义生成：
+// 2. 自定义生成，默认生成的 links 会指向 __root__
 const graph2 = genGraph({
-  __root__: {
-    version: '0.0.1',
-    dependencies: {
-      axios: '1.0.0',
-      vue: '3.0.0',
-    },
-    homepage: '',
+  version: '0.0.1',
+  dependencies: {
+    axios: '1.0.0',
+    vue: '3.0.0',
   },
+  homepage: '',
 })
 console.log(graph2)
+
+// 3. 自定义生成，指定此 links 指向
+
+const graph3 = genGraph({
+  version: '0.0.1',
+  dependencies: {
+    axios: '1.0.0',
+    vue: '3.0.0',
+  },
+  homepage: '',
+}, 'vite')
+console.log(graph3)
+
+// 自定义生成出的 nodes 可能会和之前的 nodes 重复，如果希望手动添加节点，建议使用 Set 数据结构判断一下节点是否已经存在
+const { nodes } = graph2
+const nodesSet = new Set(nodes.map(item => item.name))
+for (let i = 0; i < nodes.length; i++) {
+  // 只有节点不存在的时候，才会向 graph3 中添加节点
+  if (!nodesSet.has(nodes[i].name))
+    graph3.nodes.push(nodes[i])
+}
