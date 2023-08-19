73ebe168d07a6d82722cf1460f0f5d0b01178ca7
Plumbiu <3434909403@qq.com>
2023-08-19 22:21:23 +0800

    fix: API更改导致测试失败

--- packages/core/src/__test__/genGraph.test.ts
+++ packages/core/src/__test__/genGraph.test.ts
@@ -5,13 +5,13 @@ import { genRelations, useReadFile } from '../relations'
 
 describe('genGraph test', () => {
   const relations = genRelations()
-  const { nodes, links } = genGraph(relations)
+  const { nodes, links } = genGraph(relations.__root__)
   test('nodes and links is not empty', () => {
     expect(nodes).toBeTruthy()
     expect(links).toBeTruthy()
   })
   test('nodes and links should connected to package.json', async () => {
-    const { name, dependencies, devDependencies } = useReadFile('./package.json')
+    const { name, dependencies, devDependencies } = useReadFile('package.json')
     const pkgs = useAssign(dependencies, devDependencies)
     const nodesNames = nodes.map(item => item.name)
     const linksNames = new Set([...links.map(item => item.target), ...links.map(item => item.source)])
