08db4d4e83157317e6e1981f04a309a41685a612
Plumbiu <3434909403@qq.com>
2023-08-21 23:06:08 +0800

    fix(web): 🧩 删除无用变量

--- packages/web/src/components/Chart.vue
+++ packages/web/src/components/Chart.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { onMounted, provide, ref } from 'vue'
 import type { Relations } from '@truth-cli/shared'
-import { useElementSize } from '@vueuse/core'
 import echarts from '../plugins/echarts'
 import { addGraphNode, addTreeNode, getPkgInfo, initChart, removeTreeNode } from '../utils/chart/index'
 import { preDealName } from '../utils/preDealName'
@@ -9,13 +8,9 @@ import { preDealName } from '../utils/preDealName'
 const relationsJSON = await fetch('relations.json')
 const relations: Relations = await relationsJSON.json()
 const pkgName = ref('__root__')
-const pkgInfo = ref<any>({ info: relations.__root__ })
-const canvasRef = ref(null)
-const { width, height } = useElementSize(canvasRef)
+const pkgInfo = ref<any>({ info: relations.__root__ })!
 provide('pkgName', pkgName)
 provide('pkgInfo', pkgInfo)
-provide('width', width)
-provide('height', height)
 
 onMounted(async () => {
   const chartDOM = echarts.init(document.getElementById('chart'))
@@ -23,7 +18,7 @@ onMounted(async () => {
   chartDOM.on('click', (params: any) => {
     const { data, seriesType, collapsed, treeAncestors } = params
     pkgName.value = preDealName(data.name)
-    pkgInfo!.value = getPkgInfo(pkgName.value)
+    pkgInfo.value = getPkgInfo(pkgName.value)
     if (seriesType === 'tree') {
       if (collapsed)
         removeTreeNode(data)
@@ -39,5 +34,5 @@ onMounted(async () => {
 
 <template>
   <Container />
-  <div id="chart" ref="canvasRef" style="padding-top: 50px;height:calc(100vh - 60px);" />
+  <div id="chart" style="padding-top: 50px;height:calc(100vh - 60px);" />
 </template>
