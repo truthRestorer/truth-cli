2da7509fa0b167b247b410677faf1967888fd928
Plumbiu <3434909403@qq.com>
2023-08-26 13:23:21 +0800

    fix(playground): 高亮问题

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,31 +1,37 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { genGraph, genTree, genVersions } from '@truth-cli/core'
-import treePkgs from './assets/pkgs.txt?raw'
+import txt from './assets/pkgs.txt?raw'
 import relations from './assets/relations.json'
-import pkgs from './assets/pkgs.json'
+import json from './assets/pkgs.json'
 
 const data = ref<any>(relations)
+const active = ref('relations')
 const graph = genGraph(relations.__root__ as any)
 const versions = genVersions(relations as any)
 const tree = genTree(3, relations as any)
+
+function toggleLgend(val: any, type: string) {
+  data.value = val
+  active.value = type
+}
 </script>
 
 <template>
   <div class="main">
     <div class="select">
       <Header />
-      <span :class="{ active: data === relations }" @click="data = relations">genRelations</span>
-      <span :class="{ active: data === graph }" @click="data = graph">genGraph</span>
-      <span :class="{ active: data === tree }" @click="data = tree">genTree</span>
-      <span :class="{ active: data === versions }" @click="data = versions">genVersions</span>
-      <span :class="{ active: data === pkgs }" @click="data = pkgs">genJson</span>
-      <span :class="{ active: data === treePkgs }" @click="data = treePkgs">genTxt</span>
+      <span :class="{ active: active === 'relations' }" @click="toggleLgend(relations, 'relations')">genRelations</span>
+      <span :class="{ active: active === 'graph' }" @click="toggleLgend(graph, 'graph')">genGraph</span>
+      <span :class="{ active: active === 'tree' }" @click="toggleLgend(tree, 'tree')">genTree</span>
+      <span :class="{ active: active === 'versions' }" @click="toggleLgend(versions, 'versions')">genVersions</span>
+      <span :class="{ active: active === 'pkgs' }" @click="toggleLgend(json, 'pkgs')">genJson</span>
+      <span :class="{ active: active === 'treePkgs' }" @click="toggleLgend(txt, 'treePkgs')">genTxt</span>
     </div>
     <div style="padding-top: 55px;">
-      <JsonView v-if="data !== treePkgs" :data="data" :depth="2" />
+      <JsonView v-if="data !== txt" :data="data" :depth="2" />
       <div v-else style="white-space: pre;">
-        {{ treePkgs }}
+        {{ txt }}
       </div>
     </div>
   </div>
