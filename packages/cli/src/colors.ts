3b29942a8ee75aa3db6f034bce1eb5c5beb75383
Plumbiu <3434909403@qq.com>
2023-08-31 18:12:26 +0800

    style: 增加换行

--- packages/cli/src/colors.ts
+++ packages/cli/src/colors.ts
@@ -5,12 +5,15 @@ export function cyan(s: string | number) {
 export function bold(s: string | number) {
   return `\x1B[1m${s}\x1B[22m`
 }
+
 export function dim(s: string | number) {
   return `\x1B[2m${s}\x1B[22m`
 }
+
 export function green(s: string | number) {
   return `\x1B[32m${s}\x1B[39m`
 }
+
 export function red(s: string | number) {
   return `\x1B[31m${s}\x1B[39m`
 }
