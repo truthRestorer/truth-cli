7b47c8f1733339c5091d86fb8f7a71db2ebb8d6b
Plumbiu <3434909403@qq.com>
2023-08-31 12:26:19 +0800

    feat(example): 完善genCirculation示例

--- example/genCirculation.ts
+++ example/genCirculation.ts
@@ -2,5 +2,21 @@
 import { genCirculation } from '@truth-cli/core'
 import { relations } from './index.js'
 
-const circulation = genCirculation(relations)
-console.log(circulation)
+// 1. 通过获取所有依赖的循环依赖
+const circulation1 = genCirculation(relations)
+console.log(circulation1)
+
+// 2. 自定义生成，默认生成的 links 会指向 __root__
+const circulation2 = genCirculation({
+  __root__: {
+    dependencies: {
+      axios: '1.0.0',
+    },
+  },
+  axios: {
+    dependencies: {
+      __root__: 'latest',
+    },
+  },
+})
+console.log(circulation2)
