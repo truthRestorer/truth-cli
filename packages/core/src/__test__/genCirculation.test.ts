7bee8fde677b36f82d9318130253ebc5105fb79c
Plumbiu <3434909403@qq.com>
2023-08-31 18:24:09 +0800

    test: 完成genCirculation API测试

---
+++ packages/core/src/__test__/genCirculation.test.ts
@@ -0,0 +1,34 @@
+import { describe, expect, test } from 'vitest'
+import { genCirculation } from '../circulation'
+
+describe('genCirculation API 测试', () => {
+  test('不指定 name 字段和 target 参数', () => {
+    const graph = genCirculation({
+      __root__: {
+        version: '1.0.0',
+        dependencies: {
+          axios: '0.0.1',
+        },
+        devDependencies: {
+          'vue': '3.0.0',
+          '@truth-cli/core': '1.0.0',
+        },
+      },
+      axios: {
+        devDependencies: {
+          __root__: 'latest',
+        },
+      },
+      vue: {
+        devDependencies: {
+          __root__: 'latest',
+        },
+      },
+    })
+    expect(graph).toEqual({
+      __root__: ['axios', 'vue'],
+      axios: ['__root__'],
+      vue: ['__root__'],
+    })
+  })
+})
