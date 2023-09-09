cb0d8d5a7a28e5cddae39d0fc8c16c8f33b7f088
Plumbiu <3434909403@qq.com>
2023-09-09 12:54:53 +0800

    fix: 删除打包文件

--- packages/cli/src/__test__/readFile.bench.ts
+++ packages/cli/src/__test__/readFile.bench.ts
@@ -1,14 +1,10 @@
 import { readFileSync } from 'node:fs'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
 import { bench } from 'vitest'
 
-const __dirname = fileURLToPath(new URL('.', import.meta.url))
-
-bench('读取压缩文件', () => {
-  readFileSync(path.resolve(__dirname, './index.html.br'))
+bench('读取未压缩文件', () => {
+  readFileSync('e2e/index.html')
 }, { time: 30 })
 
-bench('读取未压缩文件', () => {
-  readFileSync(path.resolve(__dirname, './index.html'))
+bench('读取压缩文件', () => {
+  readFileSync('packages/cli/dist/index.html.br')
 }, { time: 30 })
