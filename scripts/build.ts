21251d402548749f47591a9c2569a03dda72de59
Plumbiu <3434909403@qq.com>
2023-09-10 22:34:56 +0800

    fix(build): 简化重复逻辑

--- scripts/build.ts
+++ scripts/build.ts
@@ -5,36 +5,33 @@ import { buildWeb } from './utils.js'
 // eslint-disable-next-line n/prefer-global/process
 const argv = minimist(process.argv.slice(2))
 const target = argv._
+const basePlugins = {
+  bundle: true,
+  minify: true,
+  platform: 'node' as esbuild.Platform,
+  format: 'esm' as esbuild.Format,
+}
 
 async function resolveBuild() {
   await esbuild.build({
+    ...basePlugins,
     entryPoints: ['packages/core/index.ts', 'packages/core/node.ts'],
-    bundle: true,
     outdir: 'packages/core/dist',
-    minify: true,
-    platform: 'node',
-    format: 'esm',
     sourcemap: true,
   })
   target.includes('web') && await buildWeb('../packages/cli', true)
   await esbuild.build({
+    ...basePlugins,
     entryPoints: ['packages/cli/index.ts'],
-    bundle: true,
     outdir: 'packages/cli/dist',
-    minify: true,
-    platform: 'node',
-    format: 'esm',
     banner: {
       js: '#! /usr/bin/env node',
     },
   })
   await esbuild.build({
+    ...basePlugins,
     entryPoints: ['packages/shared/index.ts'],
-    bundle: true,
     outdir: 'packages/shared/dist',
-    minify: true,
-    platform: 'node',
-    format: 'esm',
   })
 }
 
