9fa8af65e0f1745769867057abe2aed8b67c09e5
Plumbiu <3434909403@qq.com>
2023-09-18 13:29:01 +0800

    fix: relations error

--- packages/cli/src/server.ts
+++ packages/cli/src/server.ts
@@ -1,7 +1,7 @@
 import { createServer } from 'node:http'
 import { readFileSync } from 'node:fs'
 import { gzipSync } from 'node:zlib'
-import { genBaseRelation, genRelations } from '@truth-cli/core/node'
+import { genRelations } from '@truth-cli/core/node'
 import { htmlPath, logError, logFinished } from './const.js'
 
 // 启动网页
@@ -11,20 +11,16 @@ export function startWebServer(port: number = 3003) {
     if (Number.isNaN(port))
       throw new TypeError('illegal type of [port]')
     const begin = Date.now()
-    const base = JSON.stringify(genBaseRelation())
     const html = readFileSync(htmlPath)
     const server = createServer((req, res) => {
       if (req.url === '/') {
         res.setHeader('content-encoding', 'br')
         res.end(html)
       }
-      else if (req.url === '/relations.json') {
+      else if (req.url === '/base.json') {
         const relations = gzipSync(JSON.stringify(genRelations()))
         res.setHeader('content-encoding', 'gzip')
         res.end(relations)
-      }
-      else if (req.url === '/base.json') {
-        res.end(base)
       }
     })
     server.on('error', (e: any) => {
